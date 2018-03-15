2d4df84c34db9fd87f785dee0a86877ed5d33cad
WLL <hao123wll@163.com>
2018-03-15 11:06:18 +0800

    first commit

---
+++ application/admin/common/Base.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WLL
+ * Date: 2018/3/14
+ * Time: 19:26
+ */
+
+namespace app\admin\common;
+
+
+use think\Controller;
+
+class Base extends Controller
+{
+
+}
---
+++ public/admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WLL
+ * Date: 2018/3/6
+ * Time: 15:50
+ */
+
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/../application/');
+// 加载框架引导文件
+require __DIR__ . '/../thinkphp/start.php';
